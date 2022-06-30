3a50e93561c11ec7ca0c65cea46f8e5434b62677
Carlmem <thealexvin12@gmail.com>
2022-06-30 20:51:07 +0200

    Revert "Simple-Chatty-Bot"
    This reverts commit ee7902b098a7bf11dd25f4da1a0d74df34533585.

--- Topics/Types and variables/Fill the declaration/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int x = 123456;
-
-        System.out.println(x);
-    }
-}
--- Topics/Types and variables/Theory/src/Task.java
+++
@@ -1,8 +0,0 @@
-// You can experiment here, it won’t be checked
-
-public class Task {
-  public static void main(String[] args) {
-    int x = 123456;
-    System.out.println("x");
-  }
-}
